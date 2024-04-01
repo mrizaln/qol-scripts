2c03ad0a36b068a78cf97cb9767bc1c8469315d3
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-04-01 15:37:54 +0700

    Update script to use better default

--- scripts/backup_bash_history.sh
+++ scripts/backup_bash_history.sh
@@ -5,6 +5,7 @@ home="/home/mrizaln"
 BASH_HIST=$home/.bash_history
 BACKUP_DIR=$home/.bash_history.d
 BACKUP="${BACKUP_DIR}/.bash_history.$(date +%Y%m%d_%H%M)"
+MAX_BACKUP=30
 
 backup()
 {
@@ -24,9 +25,9 @@ backup()
     cp "$BASH_HIST" "$BACKUP"
     gzip "$BACKUP"
 
-    # only make 30 copies
-    if [[ $(ls "$BACKUP_DIR/.bash_history"* | wc -l) -ge 30 ]]; then
-        ls -t "$BACKUP_DIR/.bash_history"* | tail -n+30 | \
+    # only make MAX_BACKUP copies
+    if [[ $(ls "$BACKUP_DIR/.bash_history"* | wc -l) -ge $MAX_BACKUP ]]; then
+        ls -t "$BACKUP_DIR/.bash_history"* | tail -n+${MAX_BACKUP} | \
         while read f; do
             echo removing $f
             rm "$f"
@@ -40,6 +41,7 @@ restore()
     if [ -z "$file" ]; then
         file=$(ls $BACKUP_DIR/.bash_history.* -rt1 | tail -n1)
     fi
+
     if ! [ -e "$file" ]; then
         echo file does not exist
         exit 1
@@ -50,27 +52,36 @@ restore()
     read -p "Are you sure you want to restore your bash history? [y/N] " ans
 
     case $ans in
-        y)
-            ;&
-        Y)
+        y | Y | yes | Yes | YES)
             ;;
         *)
+            echo "Aborted"
             exit 0;
             ;;
     esac
 
-    cp "$file" /tmp/.bash_history.gz
-    gunzip /tmp/.bash_history.gz
-    rm $HOME/.bash_history
-    mv /tmp/.bash_history $HOME
+    temp_dir=$(mktemp -d)
+    cp "$file" "$temp_dir/.bash_history.gz"
+    gunzip "$temp_dir/.bash_history.gz"
+    rm "$BASH_HIST"
+    mv "$temp_dir/.bash_history" "$BASH_HIST"
+    rm -r "$temp_dir"
+
+    echo "Restored from $file to $BASH_HIST"
 }
 
+main()
+{
+    if [[ $1 == "--restore" ]]; then
+        restore $2
+    elif [[ $1 == "--force-backup" ]]; then
+        backup --force
+    elif [[ $1 == "--backup" ]]; then
+        backup
+    else
+        echo "Usage: $0 [--backup] [--restore [file]] [--force-backup]"
+        exit 0
+    fi
+}
 
-
-if [[ $1 == "--restore" ]]; then
-    restore $2
-elif [[ $1 == "--force-backup" ]]; then
-    backup --force
-else
-    backup
-fi
+main "$@"
